ef5f089cf96c447a741c5eb6e5f533553dbd13ed
NAME <gbgo77@naver.com>
2020-08-17 11:42:58 +0900

    full calendar fork and custom.. ing

---
+++ react-redux/src/Test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+  const top = 50 ;
+  const left = 50 ;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function Test(props) {
+
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+  return (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Study List</h2>
+      <p id="simple-modal-description">
+        <li>Java Study / 16:00~18:00 / (1 / 5) / <a href='/'>join</a></li>
+        <li>React Study / 16:00~18:00 / (1 / 5) / <a href='/'>join</a></li>
+        <li>모각코 / 16:00~18:00 / (1 / 5) / <a href='/'>join</a></li>
+      </p>
+    </div>
+  );
+}
